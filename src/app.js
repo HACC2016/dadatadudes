e3057895030d073359720452f24e5456b13a5f8b
kawika <kawikakekahuna@gmail.com>
2016-09-10 16:12:35 -1000

    Removed unused module imports.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,5 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  TouchableHighlight,
-  Navigator
-} from 'react-native';
-import {
-  Router
-} from 'react-native-router-flux';
+import { Router } from 'react-native-router-flux';
 import scenes from './routes/index.js';
 
 class App extends Component {
