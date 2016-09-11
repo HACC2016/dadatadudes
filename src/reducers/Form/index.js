6608838ffcb42badda73abc82180c833d5a2fc98
Bryan Butteling <Boots@Bryans-MacBook-Air-2.local>
2016-09-10 15:30:33 -1000

    Attached VISPDAT view to redux store

--- src/reducers/Form/index.js
+++ src/reducers/Form/index.js
@@ -1,7 +1,8 @@
 import { actions } from '../../actions/Form/index.js';
 
 const submitForm = (state, action) => {
-  console.log(action.data);
+  console.log('action', action.data);
+  return state;
 };
 
 export default function reducer(state = {}, action = {}) {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import routes from './routes.js';
+import formInputs from './Form/index.js';
 // ... other reducers
 
 export default combineReducers({
-  routes
+  routes,
+  formInputs
 });
