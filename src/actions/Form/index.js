5fb3d5d9b2ec9aaa868acb57f18eb0c8630e2ebf
kawika <kawikakekahuna@gmail.com>
2016-09-10 19:21:24 -1000

    Created action creators for adding dynamic fields to the redux store.  Deleted files.     - Removed TextFields.     - Added addFormField to Form actions.     - PointInTime.js will need to be changed when     form input components are completed. (radio, select, etc..)

--- src/actions/Form/index.js
+++ src/actions/Form/index.js
@@ -1,6 +1,12 @@
 export const actions = {
+  ADD_FORM_FIELD: 'ADD_FORM_FIELD',
   SUBMIT_FORM: 'SUBMIT_FORM'
 };
+
+export const addFormField = (data) => ({
+  type: actions.ADD_FORM_FIELD,
+  data
+});
 
 export const submitForm = (data) => ({
   type: actions.SUBMIT_FORM,
