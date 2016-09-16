8a2423aec7bab4dc0a3a73849a96ff8bea8409a0
Bryan Butteling <Boots@Bryans-MacBook-Air-2.local>
2016-09-15 23:51:00 -1000

    Dis radio button faka like finally work...cheeeee

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,91 +1,48 @@
-// ***NEED TO FIGURE OUT HOW TO SWITCH ON AND OFF AFTER SELECTION***
-
 import React, { Component, PropTypes } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { MKRadioButton, MKColor } from 'react-native-material-kit';
 
-// const styles = Object.assign({}, StyleSheet.create({
+const styles = Object.assign({}, StyleSheet.create({
 
-//   row: {
-//     flexDirection: 'row'
-//   },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 
-//   col: {
-//     flex: 1,
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//     marginLeft: 20, marginRight: 20
-//   },
+}));
 
-//   titleText: {
-//     fontSize: 15,
-//     fontWeight: 'bold'
-//   }
-
-// }));
-
-class RadioOptions extends Component {
+class RadioButton extends Component {
 
   static propTypes = {
+    group: PropTypes.object.isRequired,
     text: PropTypes.string.isRequired
   }
 
   constructor(props) {
     super(props);
-    this.radioGroup = new MKRadioButton.Group();
+  }
+
+  shouldComponentUpdate(nextProps) {
+    console.log('RADIO BUTTON next props', nextProps);
+    return false;
   }
 
   render() {
+    const { text, group } = this.props;
     return (
-      <View>
+      <View style={styles.container}>
         <MKRadioButton
-          group={this.radioGroup}
+          group={group}
           fillColor={MKColor.Teal}
           borderOnColor={MKColor.Pink}
           borderOffColor={MKColor.Green}
           rippleColor={MKColor.Blue}
         />
-        <Text>{this.props.text}</Text>
+        <Text>{text}</Text>
       </View>
     );
-    // return (
-    //   <View style={styles.row}>
-    //     <Text style={styles.titleText}>
-    //     {this.props.radioTitle}
-    //     </Text>
-    //     <View style={styles.col}>
-    //       <MKRadioButton
-    //         group={this.radioGroup}
-    //         fillColor={MKColor.Teal}
-    //         borderOnColor={MKColor.Pink}
-    //         borderOffColor={MKColor.Green}
-    //         rippleColor={MKColor.Blue}
-    //       />
-    //       <Text>Yes</Text>
-    //     </View>
-    //     <View style={styles.col}>
-    //       <MKRadioButton
-    //         group={this.radioGroup}
-    //         fillColor={MKColor.Teal}
-    //         borderOnColor={MKColor.Pink}
-    //         borderOffColor={MKColor.Green}
-    //         rippleColor={MKColor.Blue}
-    //       />
-    //       <Text>No</Text>
-    //     </View>
-    //     <View style={styles.col}>
-    //       <MKRadioButton
-    //         group={this.radioGroup}
-    //         fillColor={MKColor.Teal}
-    //         borderOnColor={MKColor.Pink}
-    //         borderOffColor={MKColor.Green}
-    //         rippleColor={MKColor.Blue}
-    //       />
-    //       <Text>Refuse</Text>
-    //     </View>
-    //   </View>
-    // );
   }
 }
 
-export default RadioOptions;
+export default RadioButton;
